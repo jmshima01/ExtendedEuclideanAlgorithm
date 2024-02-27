6f2cec9cfe35f8c54d79cbdeef4f38ed31f32645
jmshima01 <jamesshima@mines.edu>
2024-02-26 21:07:49 -0700

    done

---
+++ aes.py
@@ -0,0 +1,32 @@
+
+
+def mult(a_x,b_x):
+    pass
+    # lcs = lambda x: x[1:] + '0'
+
+    # p_x = "100011011"
+    # p_v = int(p_x,2)
+    
+    # a_v = int(a_x,2)
+    # b_v = int(b_x,2)
+
+    # print(p_v)
+    # print(bin(p_v))
+
+    # h = lambda x: lcs(x) if x[0]=='0' else bin(int(lcs(a_x),2) ^ int("00011011",2))[2:]
+    
+    # to_xor = []        
+    # for i,v in enumerate(b_x):
+    #     i = 7-i
+    #     if v == '1':
+    #         r = a_x
+    #         for k in range(i-1):
+    #             r = h(r)    
+    #         to_xor.append(r)
+    
+
+    # res = int(to_xor[0],2)
+    # for i in range(1,len(to_xor)):
+    #     res ^= int(to_xor[i],2)
+    
+    # print(bin(res))
--- eea.py
+++ eea.py
@@ -1,15 +1,13 @@
 from sys import argv
 
-
 def eea(a,b):
-
     x = [1,0]
     y = [0,1]
     r = [a,b]
     a_l = [a]
     b_l = [b]
     q = [None,None]
-    while (r[-1] >= 1):
+    while (r[-1] > 1):
         q.append(a // b)
         r.append(a % b)
         x.append(x[-2] - q[-1]*x[-1])
@@ -24,30 +22,29 @@ def eea(a,b):
     print(f"r: {r}")
     print(f"x: {x}")
     print(f"y: {y}")
+    return x[-1]
 
-    
 def gcd_(a,b):
-    # if(a<b):
-    #     a,b=b,a
     if(b==0):
         return a
     else:
         return gcd_(b,a%b)
 
-
 if __name__ == "__main__":
     if len(argv) < 3:
-        print("Usage: python3 main.py a b")
+        print("Usage: python3 eea.py a b")
         exit(1)
     
     a = int(argv[1])
     b = int(argv[2])
-
-    eea(a,b)
-    
-    
-    print("============")
-    print(gcd_(a,b))
+    print(f"gcd({a},{b}) =",gcd_(a,b))
     if gcd_(a,b) != 1:
-        print(f"NO INVERSE: gcd = {gcd_(a,b)}")
-        exit(1)
+        print("NO INVERSE\n")
+        exit(0)
+    print("============")
+    result = eea(a,b)
+    while result < 0:
+        result+=b
+    print("============")
+    print("result: ",result)
+    print()
